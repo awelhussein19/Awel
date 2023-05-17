f1394f8f1dcf14b4e303f9a91de0abd632a6995f
awelhussein19 <129486109+awelhussein19@users.noreply.github.com>
2023-05-17 15:50:11 +0300

    Add files via upload
    It's good project about Quiz App.So it's very appriciated

---
+++ APP/quiz.js
@@ -0,0 +1,205 @@
+let timeLeft = document.querySelector(".time-left");
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestion = document.querySelector(".number-of-question");
+let displayContainer = document.getElementById("display-container");
+let scoreContainer = document.querySelector(".score-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector(".start-screen");
+let startButton = document.getElementById("start-button");
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
+//Questions and Options array
+const quizArray = [
+    {
+        id: "0",
+        question: "What is the largest lake in the world?",
+        options: ["Nayil", "Ganale", "Baro", "Wabishabale"],
+        correct: "Nayil",
+    },
+    {
+        id: "1",
+        question: "Which one of the following largest continent in world?",
+        options: [" Asia", "America", "Africa", "Awustralia"],
+        correct: "Asia",
+    },
+    {
+        id: "2",
+        question: "Which one is the largest mountain in the world?",
+        options: ["Ras-Dashen Mountain", "Cilalo Mountain", "Kilimanjaro Mountain", "Tullu-Dimtuu Mountain"],
+        correct: "Kilimanjaro Mountain",
+    },
+    {
+        id: "3",
+        question: "Which is the largest animal in the world?",
+        options: ["Elephant", "Lion", "Chetah", "Red-fox"],
+        correct: "Elephant",
+    },
+    {
+        id: "4",
+        question: "Which of the following fast animal in the word.",
+        options: ["Rabbit", "Dog", "Cat", "Sheep"],
+        correct: "Rabbit",
+    },
+    {
+        id: "5",
+        question: "Which of the following best university in Ethiopia?:",
+        options: ["Addis-Ababa University", "Arsi University", "Jima University", "Odaa-Bultum"],
+        correct: "Addis Ababa University",
+    }, {
+        id: "6",
+        question: "How can you catch a computer virus?",
+        options: ["Sending e-mail messages", "Using a laptop during the winter", "Opening e-mail attachments", "Shopping on-line"],
+        correct: "Opening e-mail attachments",
+    },
+    {
+        id: "7",
+        question: "Which is The largest Region in Ethiopia?",
+        options: ["Oromia region", "Sumale Region", "Amara Region", "Tigray Region"],
+        correct: "Oromia Region",
+    },
+    {
+        id: "8",
+        question: "Which is the current prime minister of Ethiopia?",
+        options: ["DR.Abiyi Ahmed", "Mr.Demeka Mokonen", "Mr.Jawar Mohammed", "MR.Shimalis Abdisa"],
+        correct: "DR.Abiyi Ahmed",
+    },
+    {
+        id: "9",
+        question: "Which of the following is Largest Country in East Africa by size?",
+        options: ["Ruwanda", "Sudan", "Kenya", "Ethiopia"],
+        correct: "Ethiopia",
+    },
+];
+//Restart Quiz
+restart.addEventListener("click", () => {
+    initial();
+    displayContainer.classList.remove("hide");
+    scoreContainer.classList.add("hide");
+});
+//Next Button
+nextBtn.addEventListener(
+    "click",
+    (displayNext = () => {
+        //increment questionCount
+        questionCount += 1;
+        //if last question
+        if (questionCount == quizArray.length) {
+            //hide question container and display score
+            displayContainer.classList.add("hide");
+            scoreContainer.classList.remove("hide");
+            //user score
+            userScore.innerHTML =
+                "Your score is " + scoreCount + " out of " + questionCount;
+        } else {
+            //display questionCount
+            countOfQuestion.innerHTML =
+                questionCount + 1 + " of " + quizArray.length + " Question";
+            //display quiz
+            quizDisplay(questionCount);
+            count = 11;
+            clearInterval(countdown);
+            timerDisplay();
+        }
+    })
+);
+//Timer
+const timerDisplay = () => {
+    countdown = setInterval(() => {
+        count--;
+        timeLeft.innerHTML = `${count}s`;
+        if (count == 0) {
+            clearInterval(countdown);
+            displayNext();
+        }
+    }, 1000);
+};
+//Display quiz
+const quizDisplay = (questionCount) => {
+    let quizCards = document.querySelectorAll(".container-mid");
+    //Hide other cards
+    quizCards.forEach((card) => {
+        card.classList.add("hide");
+    });
+    //display current question card
+    quizCards[questionCount].classList.remove("hide");
+};
+//Quiz Creation
+function quizCreator() {
+    //randomly sort questions
+    quizArray.sort(() => Math.random() - 0.5);
+    //generate quiz
+    for (let i of quizArray) {
+        //randomly sort options
+        i.options.sort(() => Math.random() - 0.5);
+        //quiz card creation
+        let div = document.createElement("div");
+        div.classList.add("container-mid", "hide");
+        //question number
+        countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+        //question
+        let question_DIV = document.createElement("p");
+        question_DIV.classList.add("question");
+        question_DIV.innerHTML = i.question;
+        div.appendChild(question_DIV);
+        //options
+        div.innerHTML += `
+    <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
+     <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
+      <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
+       <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
+    `;
+        quizContainer.appendChild(div);
+    }
+}
+//Checker Function to check if option is correct or not
+function checker(userOption) {
+    let userSolution = userOption.innerText;
+    let question =
+        document.getElementsByClassName("container-mid")[questionCount];
+    let options = question.querySelectorAll(".option-div");
+    //if user clicked answer == correct option stored in object
+    if (userSolution === quizArray[questionCount].correct) {
+        userOption.classList.add("correct");
+        scoreCount++;
+    } else {
+        userOption.classList.add("incorrect");
+        //For marking the correct option
+        options.forEach((element) => {
+            if (element.innerText == quizArray[questionCount].correct) {
+                element.classList.add("correct");
+            }
+        });
+    }
+    //clear interval(stop timer)
+    clearInterval(countdown);
+    //disable all options
+    options.forEach((element) => {
+        element.disabled = true;
+    });
+}
+//initial setup
+function initial() {
+    quizContainer.innerHTML = "";
+    questionCount = 0;
+    scoreCount = 0;
+    count = 11;
+    clearInterval(countdown);
+    timerDisplay();
+    quizCreator();
+    quizDisplay(questionCount);
+}
+//when user click on start button
+startButton.addEventListener("click", () => {
+    startScreen.classList.add("hide");
+    displayContainer.classList.remove("hide");
+    initial();
+});
+//hide quiz and display start screen
+window.onload = () => {
+    startScreen.classList.remove("hide");
+    displayContainer.classList.add("hide");
+};
